d982d5c0ead86e915ecafa56acfb65f489d9bcf2
yan <372387016@qq.com>
2017-07-28 20:08:09 +0800

    Finish user signup

--- resources/lang/en/validation.php
+++ resources/lang/en/validation.php
@@ -92,6 +92,9 @@ return [
         'attribute-name' => [
             'rule-name' => 'custom-message',
         ],
+        'email' => [
+            'required' => '邮箱地址不能为空！',
+        ]
     ],
 
     /*
@@ -106,5 +109,4 @@ return [
     */
 
     'attributes' => [],
-
 ];
